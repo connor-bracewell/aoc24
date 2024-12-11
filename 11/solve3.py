b8466de08626813276dd170d8f2dcfa2bc600c11
Connor Bracewell <connor.bracewell@gmail.com>
2024-12-10 23:50:54 -0800

    Tidy solve3 a little

--- 11/solve3.py
+++ 11/solve3.py
@@ -1,6 +1,8 @@
 import sys
 from math import log10
 
+with open(sys.argv[1]) as f:
+  init = [int(s) for s in f.readline().split()]
 reps = int(sys.argv[2]) if len(sys.argv) > 2 else 75
 
 childcache = {0: [1]}
@@ -16,22 +18,17 @@ def children(n):
       childcache[n] = [n*2024]
   return childcache[n]
 
-with open(sys.argv[1]) as f:
-  init = [int(s) for s in f.readline().split()]
-
 todo = set(init)
-for it in range(reps+1):
+for it in range(reps):
   ntodo = set()
   for n in todo:
     valcache[it][n] = None
     ntodo.update(children(n))
   todo = ntodo
-
-for n in valcache[reps]:
+for n in todo:
   valcache[reps][n] = 1
 for it in reversed(range(reps)):
   for n in valcache[it]:
     valcache[it][n] = sum([valcache[it+1][c] for c in childcache[n]])
 
-result = sum([valcache[0][n] for n in init])
-print(result)
+print(sum([valcache[0][n] for n in init]))
