edbaa6aa50f177932cbdbc7c8f1bf5ee3e040d0b
Connor Bracewell <connor.bracewell@gmail.com>
2024-12-11 00:48:20 -0800

    Tidy up solve1

--- 01/solve1.py
+++ 01/solve1.py
@@ -1,17 +1,10 @@
 import sys
 
-list_l = []
-list_r = []
+list_l,list_r = [],[]
 with open(sys.argv[1]) as f:
   for line in f:
-    print(line)
-    l, r = line.split()
+    print(line.strip())
+    l,r = line.split()
     list_l.append(int(l))
     list_r.append(int(r))
-
-result = 0
-list_l.sort()
-list_r.sort()
-for l,r in zip(list_l, list_r):
-  result += abs(l-r)
-print(result)
+print(sum([abs(l-r) for l,r in zip(sorted(list_l),sorted(list_r))]))
