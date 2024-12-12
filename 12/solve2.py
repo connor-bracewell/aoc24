ce7a5d0cb7a1416b1dc2d0704f9586f29f04c2a2
Connor Bracewell <connor.bracewell@gmail.com>
2024-12-11 22:13:59 -0800

    Revise day 12 part 2 a little

--- 12/solve2.py
+++ 12/solve2.py
@@ -3,77 +3,97 @@ import sys
 with open(sys.argv[1]) as f:
   grid = [[c for c in line.strip()] for line in f]
 visited =[[None for _ in row] for row in grid]
+
 def iny(y):
   return 0<=y<len(grid)
 def inx(x):
   return 0<=x<len(grid[0])
+
+dw,de,dn,ds = 0,1,2,3
+
+def sample(y,x,d):
+  if d == dw:
+    return grid[y][x-1] if inx(x-1) else None
+  if d == de:
+    return grid[y][x+1] if inx(x+1) else None
+  if d == dn:
+    return grid[y-1][x] if iny(y-1) else None
+  return grid[y+1][x] if iny(y+1) else None
+
+def anyvisit(y,x,d):
+  if d == dw:
+    return inx(x-1) and visited[y][x-1]
+  if d == de:
+    return inx(x+1) and visited[y][x+1]
+  if d == dn:
+    return iny(y-1) and visited[y-1][x]
+  return iny(y+1) and visited[y+1][x]
+
+def samplev(y,x,d,d2):
+  if d == dw:
+    return inx(x-1) and visited[y][x-1] and visited[y][x-1][d2]
+  if d == de:
+    return inx(x+1) and visited[y][x+1] and visited[y][x+1][d2]
+  if d == dn:
+    return iny(y-1) and visited[y-1][x] and visited[y-1][x][d2]
+  return iny(y+1) and visited[y+1][x] and visited[y+1][x][d2]
+
+def rell(d):
+  if d == dw:
+    return dn
+  if d == de:
+    return ds
+  if d == dn:
+    return de
+  return dw
+
+def relr(d):
+  if d == dw:
+    return ds
+  if d == de:
+    return dn
+  if d == dn:
+    return dw
+  return de
+
+def adjresult(y,x,d,c):
+  findl = sample(y,x,rell(d)) == c and samplev(y,x,rell(d),d)
+  findr = sample(y,x,relr(d)) == c and samplev(y,x,relr(d),d)
+  if findl and findr: return -1
+  if findl or findr: return 0
+  return 1
+
+def explore3(y,x,d):
+  if d == dw:
+    return explore(y,x-1)
+  if d == de:
+    return explore(y,x+1)
+  if d == dn:
+    return explore(y-1,x)
+  return explore(y+1,x)
+
+# returns the unexplored [area,perimeter] of the region
+# containing the unexplored point (y,x).
 def explore(y,x):
-  visited[y][x] = [False,False,False,False]  # w,e,n,s
+  if visited[y][x]:
+    return (0,0)
+  # visited: is there a fence to the west/east/north/south of here
+  visited[y][x] = [False,False,False,False]
   c = grid[y][x]
   area = 1
   perim = 0
-  if x==0 or grid[y][x-1] != c:
-    visited[y][x][0] = True
-    seen = 0
-    if iny(y-1) and grid[y-1][x] == c and visited[y-1][x] and visited[y-1][x][0]:
-      seen += 1
-    if iny(y+1) and grid[y+1][x] == c and visited[y+1][x] and visited[y+1][x][0]:
-      seen +=1
-    if seen == 2:
-      perim -= 1
-    elif seen == 0:
-      if c == 'R': print(f'new edge west of {c} at {y},{x}')
-      perim += 1
-  if x==len(grid[0])-1 or grid[y][x+1] != c:
-    visited[y][x][1] = True
-    seen = 0
-    if iny(y-1) and grid[y-1][x] == c and visited[y-1][x] and visited[y-1][x][1]:
-      seen += 1
-    if iny(y+1) and grid[y+1][x] == c and visited[y+1][x] and visited[y+1][x][1]:
-      seen += 1
-    if seen == 2:
-      perim -= 1
-    elif seen == 0:
-      if c == 'R': print(f'new edge east of {c} at {y},{x}')
-      perim += 1
-  if y==0 or grid[y-1][x] != c:
-    visited[y][x][2] = True
-    seen = 0
-    if inx(x-1) and grid[y][x-1] == c and visited[y][x-1] and visited[y][x-1][2]:
-      seen += 1
-    if inx(x+1) and grid[y][x+1] == c and visited[y][x+1] and visited[y][x+1][2]:
-      seen += 1
-    if seen == 2:
-      perim -= 1
-    elif seen == 0:
-      if c == 'R': print(f'new edge north of {c} at {y},{x}')
-      perim += 1
-  if y==len(grid)-1 or grid[y+1][x] != c:
-    visited[y][x][3] = True 
-    seen = 0
-    if inx(x-1) and grid[y][x-1] == c and visited[y][x-1] and visited[y][x-1][3]:
-      seen += 1
-    if inx(x+1) and grid[y][x+1] == c and visited[y][x+1] and visited[y][x+1][3]:
-      seen += 1
-    if seen == 2:
-      perim -= 1
-    elif seen == 0:
-      if c == 'R': print(f'new edge south of {c} at {y},{x}')
-      perim += 1
-  if (not visited[y][x][0]) and not visited[y][x-1]:
-    res = explore(y,x-1)
-    area += res[0]
-    perim += res[1]
-  if (not visited[y][x][1]) and not visited[y][x+1]:
-    res = explore(y,x+1)
-    area += res[0]
-    perim += res[1]
-  if (not visited[y][x][2]) and not visited[y-1][x]:
-    res = explore(y-1,x)
-    area += res[0]
-    perim += res[1]
-  if (not visited[y][x][3]) and not visited[y+1][x]:
-    res = explore(y+1,x)
+  # for each direction, check if a fence is needed
+  # and update perim appropriately
+  conts = []
+  for d in [dw,de,dn,ds]:
+    if sample(y,x,d) != c:
+      visited[y][x][d] = True
+      perim += adjresult(y,x,d,c)
+    else:
+      conts.append(d)
+  # for each adjacent square of the same type, explore again
+  for d in conts:
+    res = explore3(y,x,d)
     area += res[0]
     perim += res[1]
   return (area, perim)
